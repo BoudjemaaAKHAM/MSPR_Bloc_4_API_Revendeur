ac421056786174f054d33ceec1ac6b574eb0840d
AdelOuldO <68519579+AdelOuldO@users.noreply.github.com>
2023-04-16 19:31:15 +0200

    Update suite.py

--- unittests/suite.py
+++ unittests/suite.py
@@ -8,8 +8,17 @@ from test import TestDummy
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestDummy))
-    runner = xmlrunner.XMLTestRunner(output='test-reports', verbosity=2)
-    runner.run(suite())
     return suite
 
 
+if __name__ == "__main__":
+    # Define the XMLTestRunner with output directory 'test-reports'
+    runner = xmlrunner.XMLTestRunner(output='test-reports', verbosity=2)
+
+    # Run the test suite using the XMLTestRunner
+    runner.run(suite())
+else:
+    runner = xmlrunner.XMLTestRunner(output='test-reports', verbosity=2)
+
+    # Run the test suite using the XMLTestRunner
+    runner.run(suite())
