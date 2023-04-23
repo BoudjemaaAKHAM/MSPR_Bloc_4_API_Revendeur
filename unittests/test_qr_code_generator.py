2256ee40688465b060f7809395a2d0cf66cfa754
BoudjemaaAKHAM <akhamboudjemaa@gmail.com>
2023-04-23 16:07:47 +0200

    fix qr code dir

--- unittests/test_qr_code_generator.py
+++ unittests/test_qr_code_generator.py
@@ -4,7 +4,7 @@ from services.qr_code_generator import generate_qr_code
 
 
 def test_generate_qr_code():
-    generate_qr_code('test', 'test', 'test')
+    generate_qr_code('test', './unittests/', 'test')
     assert True
 
 
